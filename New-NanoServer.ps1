d779a683788707e35967bc0d3223d7373f15d399
dave-hernandez <dhernandez@tuta.io>
2016-03-30 11:52:19 -0400

    First Public
    Requires to be ran as Administrator from a domain-joined computer.
    Automatically binds the new VM to the domain.

---
+++ New-NanoServer.ps1
@@ -0,0 +1,10 @@
+﻿$isopath = Read-Host "Enter the full path of your Windows Server 2016 Technical Preview ISO"
+$srvname = Read-Host "Enter the desired hostname of the server"
+$domname = Read-Host "Verify the domain name to join" 
+
+$mntimag = Mount-DiskImage -ImagePath $isopath -PassThru
+$isodriv = ($mntimag | Get-Volume).driveletter
+
+Import-Module ("$isodriv" + ":\NanoServer\NanoServerImageGenerator.psm1") -Verbose
+
+New-NanoServerImage -MediaPath ("$isodriv" + ":\") -BasePath .\Base -TargetPath (".\NanoServerVM\" + "$srvname" + "_C.vhdx") -ComputerName $srvname -DomainName $domname -GuestDrivers
